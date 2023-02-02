8db58b0f2eec48cca733e2d7d36438549da77d80
els0r <lennart.elsen@gmail.com>
2023-02-02 16:56:38 -0500

    TRIVIAL: simplify and normalize IP Protocol lookup

--- addon/serialize_ipprot_list.sh
+++ addon/serialize_ipprot_list.sh
@@ -12,7 +12,7 @@ egrep -v "^#" /etc/protocols | egrep -v "^ip\s+0\s+IP" | egrep -v "^(\s+)?$" | s
 echo -e "  255: \"UNKNOWN\",\n}\n\nfunc GetIPProto(id int) string {\n  return IPProtocols[id]\n}\n"
 
 echo -e "\nvar IPProtocolIDs = map[string] int {"
-egrep -v "^#" /etc/protocols | egrep -v "^ip\s+0\s+IP" | egrep -v "^(\s+)?$" | grep -v "for experimentation and testing" | sort -unk2 | awk '{print "  \"" $3 "\": " $2 ","}'
-echo -e "  \"UNKNOWN\": 255,\n}\n\nfunc GetIPProtoID(name string) (uint64, bool) {\n  ret, ok := IPProtocolIDs[name]\n  return uint64(ret), ok\n}\n"
+egrep -v "^#" /etc/protocols | egrep -v "^ip\s+0\s+IP" | egrep -v "^(\s+)?$" | grep -v "for experimentation and testing" | sort -unk2 | awk '{print "  \"" tolower($3) "\": " $2 ","}'
+echo -e "  \"unknown\": 255,\n}\n\nfunc GetIPProtoID(name string) (uint64, bool) {\n  name = strings.ToLower(strings.TrimSpace(name))\n  ret, ok := IPProtocolIDs[name]\n  return uint64(ret), ok\n}\n"
 
 exit 0
