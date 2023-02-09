15efb3bd2e08dc5436eb6f6c95ebb3d757eaf34e
Lennart Elsen <lelsen@open-systems.com>
2023-02-09 16:18:40 -0500

    TRIVIAL: more readable protocol serialization

--- addon/serialize_ipprot_list.sh
+++ addon/serialize_ipprot_list.sh
@@ -6,13 +6,30 @@ elif [[ $( uname -a | grep -i linux ) ]]; then
 	echo -e "// +build linux\n"
 fi
 
-
-echo -e "package goDB\n\nvar IPProtocols = map[int] string {"
+echo -e "package protocols"
+echo -e ""
+echo -e 'import "strings"'
+echo -e ""
+echo -e "var IPProtocols = map[int] string {"
 egrep -v "^#" /etc/protocols | egrep -v "^ip\s+0\s+IP" | egrep -v "^(\s+)?$" | sort -unk2 | awk '{print "  " $2 ": \"" $3 "\","}'
-echo -e "  255: \"UNKNOWN\",\n}\n\nfunc GetIPProto(id int) string {\n  return IPProtocols[id]\n}\n"
-
-echo -e "\nvar IPProtocolIDs = map[string] int {"
+echo -e "  255: \"UNKNOWN\","
+echo -e "}"
+echo -e ""
+echo -e ""
+echo -e "func GetIPProto(id int) string {"
+echo -e "    return IPProtocols[id]"
+echo -e "  }"
+echo -e ""
+echo -e ""
+echo -e "var IPProtocolIDs = map[string] int {"
 egrep -v "^#" /etc/protocols | egrep -v "^ip\s+0\s+IP" | egrep -v "^(\s+)?$" | grep -v "for experimentation and testing" | sort -unk2 | awk '{print "  \"" tolower($3) "\": " $2 ","}'
-echo -e "  \"unknown\": 255,\n}\n\nfunc GetIPProtoID(name string) (uint64, bool) {\n  name = strings.ToLower(strings.TrimSpace(name))\n  ret, ok := IPProtocolIDs[name]\n  return uint64(ret), ok\n}\n"
+echo -e "  \"unknown\": 255,"
+echo -e "}"
+echo -e "func GetIPProtoID(name string) (uint64, bool) {"
+echo -e "  name = strings.ToLower(strings.TrimSpace(name))"
+echo -e "  ret, ok := IPProtocolIDs[name]"
+echo -e "  return uint64(ret), ok"
+echo -e "}"
+echo -e ""
 
 exit 0
