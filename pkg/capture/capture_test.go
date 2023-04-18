b575cb60fcb10b5bfd9f294f9174762f6616e56d
fako1024 <mail+github@f-ko.eu>
2023-04-18 15:21:54 +0200

    Add exception for some tests from race detection

--- pkg/capture/capture_test.go
+++ pkg/capture/capture_test.go
@@ -2,7 +2,6 @@ package capture
 
 import (
 	"context"
-	"fmt"
 	"net"
 	"sync"
 	"testing"
@@ -13,48 +12,6 @@ import (
 	"github.com/fako1024/slimcap/link"
 	"github.com/stretchr/testify/require"
 )
-
-func TestLowTrafficDeadlock(t *testing.T) {
-	testDeadlock(t, 0)
-	testDeadlock(t, 1)
-	testDeadlock(t, 10)
-}
-
-func TestHighTrafficDeadlock(t *testing.T) {
-	testDeadlock(t, -1)
-}
-
-func TestMockPacketCapturePerformance(t *testing.T) {
-
-	if testing.Short() {
-		t.SkipNow()
-	}
-
-	testPacket, err := genDummyPacket()
-	require.Nil(t, err)
-
-	mockSrc, err := afring.NewMockSource("mock",
-		afring.CaptureLength(link.CaptureLengthMinimalIPv4Transport),
-	)
-	require.Nil(t, err)
-	mockC := newMockCapture(mockSrc)
-
-	for mockSrc.CanAddPackets() {
-		mockSrc.AddPacket(testPacket)
-	}
-	mockSrc.RunNoDrain(time.Microsecond)
-
-	runtime := 10 * time.Second
-	time.AfterFunc(runtime, func() {
-		require.Nil(t, mockSrc.Close())
-		require.Nil(t, mockSrc.Free())
-	})
-
-	mockC.process()
-	for _, v := range mockC.flowLog.flowMap {
-		fmt.Printf("Packets processed after %v: %d (%v/pkt)\n", runtime, v.packetsSent, runtime/time.Duration(v.packetsSent))
-	}
-}
 
 func testDeadlock(t *testing.T, maxPkts int) {
 
