bd55a5995aea88160a804f8dbd8237ed4b7292d0
Lennart Elsen <lelsen@open-systems.com>
2023-10-03 10:33:21 -0400

    Also make sure that internal plugins are registered

--- cmd/global-query/cmd/init_querier.go
+++ cmd/global-query/cmd/init_querier.go
@@ -7,6 +7,9 @@ import (
 	"github.com/els0r/goProbe/cmd/global-query/pkg/distributed"
 	"github.com/els0r/goProbe/plugins"
 	"github.com/spf13/viper"
+
+	// internal plugin support
+	_ "github.com/els0r/goProbe/plugins/querier"
 )
 
 func initQuerier(ctx context.Context) (querier distributed.Querier, err error) {
