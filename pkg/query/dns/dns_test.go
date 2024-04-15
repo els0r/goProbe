9969883eec840f9e47d50af4e3f31c71dcfee587
Lennart Elsen <lelsen@open-systems.com>
2024-04-15 20:10:37 +0000

    Fix DNS test

--- pkg/query/dns/dns_test.go
+++ pkg/query/dns/dns_test.go
@@ -11,6 +11,7 @@
 package dns
 
 import (
+	"context"
 	"os"
 	"testing"
 	"time"
@@ -28,7 +29,7 @@ func TestLookup(t *testing.T) {
 
 	// 8.8.8.8 is google's DNS server. This lookup should yield the same
 	// result for many years.
-	ips2domains := TimedReverseLookup([]string{"8.8.8.8", "0.0.0.0"}, 2*time.Second)
+	ips2domains := TimedReverseLookup(context.Background(), []string{"8.8.8.8", "0.0.0.0"}, 2*time.Second)
 	if domain, ok := ips2domains["8.8.8.8"]; ok && domain != "dns.google." {
 		t.Fatalf("RDNS lookup yielded wrong result: %s", domain)
 	} else if !ok {
@@ -46,7 +47,7 @@ func TestTimeout(t *testing.T) {
 	t.Parallel()
 
 	t0 := time.Now()
-	_ = TimedReverseLookup([]string{"8.8.8.8", "8.8.4.4", "192.168.0.1", "10.0.0.1", "129.3.4.5"}, 1*time.Millisecond)
+	_ = TimedReverseLookup(context.Background(), []string{"8.8.8.8", "8.8.4.4", "192.168.0.1", "10.0.0.1", "129.3.4.5"}, 1*time.Millisecond)
 	t1 := time.Now()
 	if t1.Sub(t0) > 10*time.Millisecond {
 		t.Fatal("Timeout failed")
